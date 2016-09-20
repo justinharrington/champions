4ca59a08603691746d0efd74eb2e593960241342
Justin Harrington <simman7@gmail.com>
2016-09-19 22:57:52 -0400

    Update types.js
    Cosmetic change for making pulling information into my spreadsheet easier

--- src/data/ids/types.js
+++ src/data/ids/types.js
@@ -1,7 +1,7 @@
 export const COSMIC = 'cosmic';
-export const TECH = 'tech';
 export const MUTANT = 'mutant';
-export const SKILL = 'skill';
-export const SCIENCE = 'science';
 export const MYSTIC = 'mystic';
+export const SCIENCE = 'science';
+export const SKILL = 'skill';
+export const TECH = 'tech';
 export const UNIVERSAL = 'universal';
